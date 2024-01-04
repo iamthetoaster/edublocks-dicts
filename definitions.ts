e8f1f85ba04b257cadf549e32994a80dcecffb92
Toaster <14080501+iamthetoaster@users.noreply.github.com>
2024-01-04 14:49:22 -0800

    initial commit - should have added one block which will act as a default dictionary constructor

---
+++ definitions.ts
@@ -0,0 +1,10 @@
+const dictionaryColor = "#15bad4";
+
+Blockly.Blocks["dict_constructor"] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("dict()");
+        this.setOutput(true, null);
+        this.setColour(dictionaryColor);
+    }
+};
---
+++ generators.ts
@@ -0,0 +1,4 @@
+Blockly.Python['dict_constructor'] = function() {
+    var code = `dict()`;
+    return [code, 0];
+};
